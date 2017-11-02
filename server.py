42f1b2f897f082bdb4813f73fa6abdcc5dbbb215
Brian Yu <brian@byu.io>
2017-11-01 21:31:02 -0400

    Update routes

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from flask import Flask, url_for, send_from_directory
+from flask import Flask, url_for, send_from_directory, send_file
 app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def hello():
     return "hello!"
 
-@app.route('/static/<path:path>')
-def send_js(path):
-    return send_from_directory('/', path)
+@app.route('/predictions')
+def get_image():
+    return send_file('predictions.png', mimetype='image/gif')
 
 if __name__ == "__main__":
     app.run()
