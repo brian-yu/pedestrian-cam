f03fc5048cc8b2378d584eb81b866282a32f86d6
Brian Yu <brian@byu.io>
2017-11-02 09:57:52 -0400

    Error catching

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-![Darknet Logo](http://pjreddie.com/media/files/darknet-black-small.png)
+#Pedestrian Cam
+### Spooky!
 
-#Darknet#
-Darknet is an open source neural network framework written in C and CUDA. It is fast, easy to install, and supports CPU and GPU computation.
-
-For more information see the [Darknet project website](http://pjreddie.com/darknet).
-
-For questions or issues please use the [Google Group](https://groups.google.com/forum/#!forum/darknet).
--- prediction.py
+++ prediction.py
@@ -10,26 +10,30 @@ import sys
 scene = sys.argv[1] if len(sys.argv) > 1 else "restaurant"
 
 while True:
-    last = time.time()
+    try:
+        last = time.time()
 
-    if scene == "restaurant":
-        cap = cv2.VideoCapture('http://84.35.225.233:83/SnapshotJPEG?Resolution=640x480&amp;amp;Quality=Clarity&amp;amp;1509566566')
-    elif scene == "bar":
-        cap = cv2.VideoCapture('http://24.103.196.243/cgi-bin/viewer/video.jpg?r=1509388702')
+        if scene == "restaurant":
+            cap = cv2.VideoCapture('http://84.35.225.233:83/SnapshotJPEG?Resolution=640x480&amp;amp;Quality=Clarity&amp;amp;1509566566')
+        elif scene == "bar":
+            cap = cv2.VideoCapture('http://24.103.196.243/cgi-bin/viewer/video.jpg?r=1509388702')
 
-    ret, frame = cap.read()
+        ret, frame = cap.read()
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB);
-    cv2.imwrite('cam.png', frame)
-    cmd = "./darknet detector test cfg/coco.data cfg/yolo.2.0.cfg yolo.2.0.weights cam.png"# -thresh .2"
-    #output = subprocess.check_output(cmd.split(), stdout=open(os.devnull, 'w'))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB);
+        cv2.imwrite('cam.png', frame)
+        cmd = "./darknet detector test cfg/coco.data cfg/yolo.2.0.cfg yolo.2.0.weights cam.png"# -thresh .2"
+        #output = subprocess.check_output(cmd.split(), stdout=open(os.devnull, 'w'))
 
-    output = subprocess.check_output(cmd.split())
+        output = subprocess.check_output(cmd.split())
 
-    output = output.decode("utf-8").split("\n")
-    
-    numPeople = len([i.split(":")[0] for i in output if i.split(":")[0] == 'person'])
-    print(output[0])
-    print("{} - {} people detected.".format(time.strftime("%d %b %Y %H:%M:%S", time.localtime()), numPeople))
-    with open("log.txt", "a") as myfile:
-        myfile.write("{},{}\n".format(last, numPeople))
+        output = output.decode("utf-8").split("\n")
+        
+        numPeople = len([i.split(":")[0] for i in output if i.split(":")[0] == 'person'])
+        print(output[0])
+        print("{} - {} people detected.".format(time.strftime("%d %b %Y %H:%M:%S", time.localtime()), numPeople))
+        with open("log.txt", "a") as myfile:
+            myfile.write("{},{}\n".format(last, numPeople))
+    except:
+        print(err)
+        continue
