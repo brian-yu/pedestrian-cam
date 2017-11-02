55b1981f5c8e2f0ed5e73e27bf923038f80851ff
Brian Yu <brian@byu.io>
2017-11-01 21:45:52 -0400

    Routing

--- server.py
+++ server.py
@@ -6,15 +6,15 @@ def hello():
     return "hello!"
 
 @app.route('/predictions')
-def get_image():
+def get_predictions():
     return send_file('predictions.png', mimetype='image/png')
 
 @app.route('/cam')
-def get_image():
+def get_cam():
     return send_file('cam.png', mimetype='image/png')
 
 @app.route('/log')
-def get_image():
+def get_log():
     return send_file('log.txt')
 
 if __name__ == "__main__":
